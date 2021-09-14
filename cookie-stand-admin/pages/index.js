1237d7a6a5545d65edc04aba6aa770063ecb69bc
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-09-14 03:48:36 +0300

    nothing much changed

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-
+import { useState } from 'react'
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Firas Next app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
